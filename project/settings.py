1b20c492c8802e8aa68aea9f21a570177ef11e9f
Kashif1Naqvi <syedkashifnaqvi14@gmail.com>
2025-10-07 10:56:09 +0500

    static changes

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3-9ei3j=_tj7mv8)5rf3(&d^0)vs$)#@_%)v^kc99da!_yyl@6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
